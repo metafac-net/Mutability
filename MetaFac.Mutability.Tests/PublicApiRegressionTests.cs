0ddfde3cd430a57a4e19779919d1d517631c7e18
lucian <lucian@metafac.net>
2024-08-12 15:12:40 +1000

    added public api regression checks

---
+++ MetaFac.Mutability.Tests/PublicApiRegressionTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+using PublicApiGenerator;
+
+namespace MetaFac.Mutability.Tests
+{
+    public class PublicApiRegressionTests
+    {
+        [Fact]
+        public void VersionCheck()
+        {
+            ThisAssembly.AssemblyVersion.Should().Be("2.3.0.0");
+        }
+
+        [Fact]
+        public async Task CheckPublicApi()
+        {
+            // act
+            var options = new ApiGeneratorOptions()
+            {
+                IncludeAssemblyAttributes = false
+            };
+            string currentApi = ApiGenerator.GeneratePublicApi(typeof(IFreezable).Assembly, options);
+
+            // assert
+            await Verifier.Verify(currentApi);
+        }
+
+    }
+}
